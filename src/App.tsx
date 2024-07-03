b5b9ded775329b21333f16c5bd1f9f10ddbc9750
Aayush Thakur <ashcold21@gmail.com>
2024-07-03 21:09:23 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div className='flex flex-col'>
+      <section className='min-h-screen'>
+        <header>
+
+        </header>
+        <main>
+
+        </main>
+      </section>
+      <h1 className="text-green-500">Hello</h1>
+      <footer></footer>
+    </div>
+  )
+}
+
+export default App
